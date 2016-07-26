f538221d8097819a907cdc8fc3c5149294c0b69d
Chris Bieneman <beanz@apple.com>
2016-07-26 13:01:51 -0700

    Fixes to submodule update script

--- scripts/update-repo.sh
+++ scripts/update-repo.sh
@@ -3,6 +3,8 @@
 count=`git rev-list --count HEAD`
 next=`expr $count + 1`
 
+git submodule init
+git submodule update
 git submodule foreach "git pull"
 git commit -a -m "Submodule update $next"
 git push
