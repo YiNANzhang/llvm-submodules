8a1ae049b12a83ba3cc7eda85bf19286ba32b4e3
Chris Bieneman <beanz@apple.com>
2016-07-29 11:45:06 -0700

    Fixing push command

--- Bots/submodule-update.groovy
+++ Bots/submodule-update.groovy
@@ -3,6 +3,6 @@
     stage 'Checkout'
     checkout([$class: 'GitSCM', branches: [[name: 'origin/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, trackingSubmodules: true, reference: '', timeout: 1000]], submoduleCfg: [], userRemoteConfigs: [[url: 'github.com:llvm-beanz/llvm-submodules.git', name: 'origin', credentialsId: '0091ce79-f8af-4e94-a1ca-1d48a35c06d3']]])
     stage 'Submodule Update'
-    sh '(git commit -a -m "Submodule update $(expr $(git rev-list --count HEAD) + 1)" && git push origin master) || true'
+    sh '(git commit -a -m "Submodule update $(expr $(git rev-list --count HEAD) + 1)" && git push git@github.com:llvm-beanz/llvm-submodules.git HEAD:master) || true'
   }
 }
